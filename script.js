db38352f696e351a118a10ef43e710cbdac01dcb
Haris Ayub <ayubh@mcmaster.ca>
2022-11-21 14:04:41 -0500

    changes

---
+++ script.js
@@ -0,0 +1,13 @@
+// var marked = false;
+// document.getElementById("toChange").onclick = updateMark();
+
+// function updateMark() {
+//     marked = true;
+//     alert('Grade for Alfonzo Sylvester updated!');
+// }
+
+// function checkMarked(){
+//     if (marked){
+//         document.getElementById("toChange").innerText = "marked"
+//     }
+// }
